716a445be8f1c214906bb8b35bcaa5939843ce5d
nionis <stivennoni77@gmail.com>
2021-02-23 23:53:16 +0200

    improve binner item

--- service/components/atoms/BannerItem.js
+++ service/components/atoms/BannerItem.js
@@ -9,7 +9,7 @@ export const BannerItem = ({
   link,
   onClick,
   src,
-  video,
+  video: videoUrl,
   width,
 }) => {
   const [isDesktop, setDesktop] = useState(true);
@@ -27,48 +27,66 @@ export const BannerItem = ({
     setDeviceType();
   }, []);
 
-  const renderContent = (link) => {
-    const ImgWrapper = () => (
-      <img alt={alt} height={height} src={src} width={width} />
-    );
-    return (
-      <div
-        className="banner-element"
-        onClick={onClick}
-        onMouseEnter={() => setShow(true)}
-        onMouseLeave={() => setShow(false)}
-      >
-        <span>{blindText}</span>
-        {!children ? (
-          video ? (
-            <div className={`embed-container`}>
-              <iframe
-                allowFullScreen
-                allow="autoplay; fullscreen"
-                className="video"
-                frameBorder="0"
-                src={`${video}?title=0&autoplay=0&byline=0&portrait=0&playsinline=0&controls=${
-                  isDesktop ? (showingControls ? 1 : 0) : 1
-                }&loop=0&app_id=122963`}
-              ></iframe>
-            </div>
-          ) : src ? (
-            link ? (
-              <a href={link} download target="_blank">
-                <ImgWrapper />
-              </a>
-            ) : (
-              <ImgWrapper />
-            )
-          ) : null
-        ) : (
-          children
-        )}
-      </div>
-    );
-  };
+  return (
+    <div
+      className="banner-element"
+      onClick={onClick}
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+    >
+      <span>{blindText}</span>
 
-  return link ? renderContent(link) : renderContent();
+      {children
+        ? { children }
+        : videoUrl
+        ? renderVimeoVideo(
+            `${videoUrl}?title=0&autoplay=0&byline=0&portrait=0&playsinline=0&loop=0&app_id=122963`,
+            showingControls
+          )
+        : src
+        ? renderDownloadVideo(link, () => (
+            <img alt={alt} height={height} src={src} width={width} />
+          ))
+        : null}
+    </div>
+  );
+};
+
+const renderVimeoVideo = (src, showingControls) => {
+  return (
+    <div className="embed-container">
+      <iframe
+        hidden={showingControls}
+        allowFullScreen
+        allow="autoplay; fullscreen"
+        className="video"
+        frameBorder="0"
+        src={`${src}&controls=0`}
+      />
+      <iframe
+        hidden={!showingControls}
+        allowFullScreen
+        allow="autoplay; fullscreen"
+        className="video"
+        frameBorder="0"
+        src={`${src}&controls=1`}
+      />
+    </div>
+  );
+};
+
+const renderDownloadVideo = (link, ImgWrapper) => {
+  return (
+    <div>
+      {link ? (
+        <a href={link} download target="_blank">
+          <ImgWrapper />
+        </a>
+      ) : (
+        <ImgWrapper />
+      )}
+    </div>
+  );
 };
 
 BannerItem.propTypes = {
